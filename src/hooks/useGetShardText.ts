a6a73ce50fdd38eaa8aa0cf6de47b30092abd44a
Bubu <2005901+BubuMVX@users.noreply.github.com>
2026-02-03 18:01:30 +0100

    Update UI

--- src/hooks/useGetShardText.ts
+++ src/hooks/useGetShardText.ts
@@ -1,16 +1,11 @@
-import { useSelector } from 'react-redux';
-
-import { MAIN_SHARD_ID, ALL_SHARDS_SHARD_ID } from 'appConstants';
-import { isMetachain } from 'helpers';
-import { useIsSovereign } from 'hooks';
-import { activeNetworkSelector } from 'redux/selectors';
-
 export const useGetShardText = () => {
-  const { name } = useSelector(activeNetworkSelector);
-  const isSovereign = useIsSovereign();
+  /*const { name } = useSelector(activeNetworkSelector);
+  const isSovereign = useIsSovereign();*/
 
-  const getShardText = (shard: number | string | undefined) => {
-    if (shard === undefined) {
+  return (shard: number | string | undefined) => {
+    return shard;
+
+    /*if (shard === undefined) {
       return '';
     }
 
@@ -40,8 +35,6 @@ export const useGetShardText = () => {
     if (Number(shard) === 0 && isSovereign) {
       return name ?? import.meta.env.VITE_APP_BRAND_NAME ?? 'Sovereign Chain';
     }
-    return `Shard ${shard}`;
+    return `Shard ${shard}`;*/
   };
-
-  return getShardText;
 };
